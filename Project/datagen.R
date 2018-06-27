d3a391543886a44f1d44210d6889e837ff359089
Chen Dong <chen_dong@hms.harvard.edu>
2018-06-26 20:07:48 -0500

    update

--- Project/datagen.R
+++ Project/datagen.R
@@ -7,13 +7,16 @@ airdata <- data_frame()
 for(i in 1:length(files)){
   dat <- read.csv(files[i]) %>%
     filter(Sample.Duration == "24 HOUR") %>%
-    select(State.Code, County.Code, Site.Num, Parameter.Code,  
+    select(State.Code, County.Code, Site.Num, POC, 
            Parameter.Name, Year, Units.of.Measure,
            Arithmetic.Mean, State.Name, County.Name,
            Latitude, Longitude, Year) %>%
     mutate(FIPS=paste0(State.Code,"-",County.Code))
-  dat <- dat[!duplicated(dat[c("State.Code", "County.Code", "Site.Num", "Parameter.Name")]),]
+  dat <- dat[!duplicated(dat[c("State.Code", "County.Code", "Site.Num", "Parameter.Name", "POC")]),]
+  dat <- dat %>% group_by(State.Code, County.Code, Site.Num, Parameter.Name, Year, 
+                          Units.of.Measure, State.Name, County.Name, FIPS, add = T) %>% 
+    summarise(Mean = mean(Arithmetic.Mean))
   airdata <- rbind(airdata,dat)
 }
 
-write.csv(airdata,"airdata.csv",row.names = F)
+write.csv(airdata,"airdata_24h.csv",row.names = F)
